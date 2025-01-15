e732497bd371da0522fe31bb54f649b650ad4dcf
serversaravanam22 <serversaravanam22@gmail.com>
2025-01-15 09:16:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7891414677:7891414677:AAHqmQ_DifBrHvboH6VkL9TVqeu_BCEgfsg")
-API_ID = int(os.environ.get("API_ID", "14307449"))
-API_HASH = os.environ.get("API_HASH", "51581b3f5f95c2c7f97efa2608d8e4f1")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8186748271:AAFDh4hdtHXSYYAPk6jBikC6eMScKK60MAQ")
+API_ID = int(os.environ.get("API_ID", "20902603"))
+API_HASH = os.environ.get("API_HASH", "79e5caa103a9e9fb0183390b4800845d")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "7035627523"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Venkat3823:Venkat3823@cluster0.ig0oc9y.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "Pream3")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6283322330"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://ganeshganesh177:17032009@sasukeuchiha.k21bh.mongodb.net/?retryWrites=true&w=majority&appName=Sasukeuchiha")
+DB_NAME = os.environ.get("DB_NAME", "Sasukeuchiha")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002253632172"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002191966684"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002161437490"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002448160632"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002262460059"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002399764589"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
@@ -29,8 +29,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "6693549185").split()):
+    ADMINS=[6283322330]
+    for x in (os.environ.get("ADMINS", "6283322330").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -59,7 +59,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
+START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʟʟᴏ {mention}\n\nI cᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇs ɪɴ sᴘᴇcɪꜰɪc cʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs cᴀɴ ᴀccᴇss ɪᴛ ꜰʀᴏᴍ sᴘᴇcɪᴀʟ ʟɪɴᴋs")
 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hᴇʟʟᴏ {mention}\n\n<b>Yᴏᴜ Nᴇᴇᴅ Tᴏ Jᴏɪɴ Iɴ Mʏ Cʜᴀɴɴᴇʟs Tᴏ Gᴇᴛ Aɴɪᴍᴇ Fɪʟᴇs\n\nKɪɴᴅʟʏ Pʟᴇᴀsᴇ Jᴏɪɴ Cʜᴀɴɴᴇʟs\n\nIғ ʏᴏᴜ ᴅᴏɴ'ᴛ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ɢᴇᴛ ғɪʟᴇ ᴄʜᴇᴄᴋ <a href=https://t.me/Tamil_Anime_Files/893>Tᴜᴛᴏʀɪᴀʟ</a></b>")
 
@@ -68,7 +68,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hᴇʟʟᴏ {mention}\n\n<b>Y�
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6848088376)
+ADMINS.append(6283322330)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
